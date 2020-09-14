6b91f211f9a293827fafd732af88c48797a84bcc
Ariel V Lima <arielflima@gmail.com>
2020-09-14 14:48:52 -0300

    start challange with configuration and useEffects

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -59,17 +59,19 @@ const Dashboard: React.FC = () => {
 
   useEffect(() => {
     async function loadFoods(): Promise<void> {
-      // Load Foods from API
+      api.get('foods').then(response => {
+        setFoods(response.data);
+      });
     }
-
     loadFoods();
   }, [selectedCategory, searchValue]);
 
   useEffect(() => {
     async function loadCategories(): Promise<void> {
-      // Load categories from API
+      api.get('categories').then(response => {
+        setCategories(response.data);
+      });
     }
-
     loadCategories();
   }, []);
 
